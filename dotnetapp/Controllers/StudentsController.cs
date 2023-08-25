233e5b11d949c0550f4734bc1283ca09bdd5fe73
Iamneo <techadmin@iamneo.ai>
2023-08-25 11:52:57 +0000

    Backup Commit

---
+++ dotnetapp/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public StudentsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+    {
+        return await _context.Students.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Student>> GetStudentById(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return student;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Student>> CreateStudent(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetStudentById), new { id = student.Id }, student);
+    }
+}
