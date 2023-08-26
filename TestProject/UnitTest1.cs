beb1c73448d47bbf868fc3aa7e2b9c177f3b6269
Iamneo <techadmin@iamneo.ai>
2023-08-26 15:03:18 +0000

    Backup Commit

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,15 +1,138 @@
-namespace TestProject;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
-public class Tests
+namespace dotnetapp.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class StudentTests
     {
-    }
+        private Type _studentType;
+        private PropertyInfo[] _studentProperties; 
+        private Type _controllerType;
+        private ApplicationDbContext _context;
+        private HttpClient _client;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _studentType = new Student().GetType();
+            _studentProperties = _studentType.GetProperties();
+            _controllerType = typeof(StudentsController);
+            _context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+
+            _client = new HttpClient();
+            _client.BaseAddress = new System.Uri("http://localhost:8080/");
+
+
+        }
+
+        [Test]
+        public async Task GetServiceCenters_ReturnsSuccess()
+        {
+            HttpResponseMessage response = await _client.GetAsync("api/Students");
+            // Assert that the response status code is 200 OK.
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // Assert that the response content is not empty.
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.IsNotEmpty(responseBody);
+        }
+
+        [Test]
+        public void Test_Student_Class_Exists()
+        {
+            Assert.NotNull(_studentType);
+        }
+
+        [Test]
+        public void Test_Student_Id_Property_DataType()
+        {
+            var idProperty = _studentProperties.FirstOrDefault(prop => prop.Name == "Id");
+            Assert.NotNull(idProperty);
+            Assert.AreEqual(typeof(int), idProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_Student_Name_Property_DataType()
+        {
+            var nameProperty = _studentProperties.FirstOrDefault(prop => prop.Name == "Name");
+            Assert.NotNull(nameProperty);
+            Assert.AreEqual(typeof(string), nameProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_Student_Department_Property_DataType()
+        {
+            var departmentProperty = _studentProperties.FirstOrDefault(prop => prop.Name == "Department");
+            Assert.NotNull(departmentProperty);
+            Assert.AreEqual(typeof(string), departmentProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_Student_PhoneNumber_Property_DataType()
+        {
+            var phoneNumberProperty = _studentProperties.FirstOrDefault(prop => prop.Name == "PhoneNumber");
+            Assert.NotNull(phoneNumberProperty);
+            Assert.AreEqual(typeof(string), phoneNumberProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_StudentsController_Class_Exists()
+        {
+            Assert.NotNull(_controllerType);
+        }
+
+        [Test]
+        public void Test_GetStudents_Method_Exists()
+        {
+            var methodInfo = _controllerType.GetMethod("GetStudents");
+            Assert.NotNull(methodInfo);
+        }
+
+        [Test]
+        public void Test_GetStudents_Method_HasHttpGetAttribute()
+        {
+            var methodInfo = _controllerType.GetMethod("GetStudents");
+            var httpGetAttribute = methodInfo.GetCustomAttributes(typeof(HttpGetAttribute), true).FirstOrDefault();
+            Assert.NotNull(httpGetAttribute);
+        }
+
+        [Test]
+        public void Test_GetStudentById_Method_Exists()
+        {
+            var methodInfo = _controllerType.GetMethod("GetStudentById");
+            Assert.NotNull(methodInfo);
+        }
+
+        [Test]
+        public void Test_GetStudentById_Method_HasHttpGetAttribute()
+        {
+            var methodInfo = _controllerType.GetMethod("GetStudentById");
+            var httpGetAttribute = methodInfo.GetCustomAttributes(typeof(HttpGetAttribute), true).FirstOrDefault();
+            Assert.NotNull(httpGetAttribute);
+        }
+
+        [Test]
+        public void Test_CreateStudent_Method_Exists()
+        {
+            var methodInfo = _controllerType.GetMethod("CreateStudent");
+            Assert.NotNull(methodInfo);
+        }
+
+        [Test]
+        public void Test_CreateStudent_Method_HasHttpPostAttribute()
+        {
+            var methodInfo = _controllerType.GetMethod("CreateStudent");
+            var httpPostAttribute = methodInfo.GetCustomAttributes(typeof(HttpPostAttribute), true).FirstOrDefault();
+            Assert.NotNull(httpPostAttribute);
+        }
     }
 }
