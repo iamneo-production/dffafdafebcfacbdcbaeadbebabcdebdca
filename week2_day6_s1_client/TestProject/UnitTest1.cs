90edaa4a1ca39216da227d406d4b646e9f4dba42
Iamneo <techadmin@iamneo.ai>
2023-08-28 09:02:40 +0000

    Backup Commit

--- week2_day6_s1_client/TestProject/UnitTest1.cs
+++ week2_day6_s1_client/TestProject/UnitTest1.cs
@@ -27,8 +27,6 @@ namespace dotnetapp.Tests
             _employeeType = new Employee().GetType();
             _deptProperties = _deptType.GetProperties();
 
-
-
             // Set up the database context before running each test
             var options = new DbContextOptionsBuilder<EMSDbContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
@@ -63,6 +61,29 @@ namespace dotnetapp.Tests
         public void Test_Employee_Class_Exists()
         {
             Assert.NotNull(_employeeType);
+        }
+        [Test]
+        public void Test_Dept_Id_Property_DataType()
+        {
+            var idProperty = _deptProperties.FirstOrDefault(prop => prop.Name == "Id");
+            Assert.NotNull(idProperty);
+            Assert.AreEqual(typeof(int), idProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_Dept_Name_Property_DataType()
+        {
+            var nameProperty = _deptProperties.FirstOrDefault(prop => prop.Name == "Name");
+            Assert.NotNull(nameProperty);
+            Assert.AreEqual(typeof(string), nameProperty.PropertyType);
+        }
+
+        [Test]
+        public void Test_Dept_Department_Property_DataType()
+        {
+            var departmentProperty = _deptProperties.FirstOrDefault(prop => prop.Name == "Location");
+            Assert.NotNull(departmentProperty);
+            Assert.AreEqual(typeof(string), departmentProperty.PropertyType);
         }
     }
 }
