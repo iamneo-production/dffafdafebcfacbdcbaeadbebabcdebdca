6228b80c6634e6592a35bf3ca2a4b6ac9300842c
Iamneo <techadmin@iamneo.ai>
2023-08-30 17:37:32 +0000

    Backup Commit

--- Week2_day6_s2_3_own/dotnetapp/Models/LibraryCard.cs
+++ Week2_day6_s2_3_own/dotnetapp/Models/LibraryCard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using dotnetapp.Attributes; // Replace with the actual namespace
 
 
 namespace dotnetapp.Models
