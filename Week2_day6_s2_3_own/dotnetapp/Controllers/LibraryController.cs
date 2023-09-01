9d64975d850365d497bc75765b0d30e2386c3c1c
Iamneo <techadmin@iamneo.ai>
2023-09-01 17:24:33 +0000

    Backup Commit

---
+++ Week2_day6_s2_3_own/dotnetapp/Controllers/LibraryController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+// using dotnetapp.Data; // Import your DbContext namespace
+using dotnetapp.Models;
+
+namespace dotnetapp.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LibraryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Implement a method to display books associated with a library card.
+        public IActionResult DisplayBooksForLibraryCard(int libraryCardId)
+        {
+            var books = _context.Books
+                .Where(b => b.LibraryCardId == libraryCardId)
+                .ToList();
+
+            return View(books);
+        }
+
+        // Implement a method to display all books in the library.
+        public IActionResult DisplayAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        // Implement a method to search for books by title.
+        public IActionResult SearchBooksByTitle(string query)
+        {
+            query = query?.Trim() ?? ""; // Trim and handle null query
+            var books = _context.Books
+                .Where(b => b.Title.Contains(query, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            return View(books);
+        }
+
+        // Implement a method to get available books.
+        public IActionResult GetAvailableBooks()
+        {
+            var availableBooks = _context.Books
+                .Where(b => b.LibraryCardId == null) // Books that are not borrowed
+                .ToList();
+
+            return View(availableBooks);
+        }
+
+        // Implement a method to get borrowed books.
+        public IActionResult GetBorrowedBooks()
+        {
+            var borrowedBooks = _context.Books
+                .Where(b => b.LibraryCardId != null) // Books that are borrowed
+                .ToList();
+
+            return View(borrowedBooks);
+        }
+    }
+}
