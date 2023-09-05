c2a10f4ca560271d198808893f67737ba69cf327
Iamneo <techadmin@iamneo.ai>
2023-09-05 03:41:01 +0000

    Backup Commit

--- BookStore-MVC-ADO/dotnetapp/TestProject/UnitTest1.cs
+++ BookStore-MVC-ADO/dotnetapp/TestProject/UnitTest1.cs
@@ -1,15 +1,44 @@
-namespace TestProject;
+using NUnit.Framework;
+using dotnetapp.Controllers;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using Moq;
+using System.Data;
+using System.Data.SqlClient;
 
-public class Tests
+
+namespace BookControllerTests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class BookControllerTests
     {
-    }
+        private BookController _controller;
+        private Type controllerType;
+        private Type carserviceType;
+        private PropertyInfo[] properties;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize the controller before each test
+            _controller = new BookController();
+            carserviceType = typeof(dotnetapp.Models.Book);
+            properties = carserviceType.GetProperties();
+            controllerType = typeof(BookController);
+        }
+
+        [Test]
+        public void Test_IndexReturns_ViewResult()
+        {
+            // Act
+            var result = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.IsAssignableFrom<List<Book>>(result.Model);
+        }
     }
 }
